480fb7b6c5a1d389e049891cf61420a5c9ed162b
juan-swxs <est.juans.munozg@unimilitar.edu.co>
2024-09-01 11:42:24 -0500

    Creacion del arreglo aleatorio

--- Algoritmos Ordenamiento/Estadistica.java
+++ Algoritmos Ordenamiento/Estadistica.java
@@ -1,7 +1,31 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Estadistica {
 
     public Estadistica(){
-        System.out.println("Hola mundol");
+        pedir_Datos();
+        
+    }
+
+    private void pedir_Datos(){
+        Scanner entrada = new Scanner(System.in);
+        Random chance = new Random();
+
+        System.out.print("Ingrese el tamaño del arreglo: ");
+        int size = entrada.nextInt();
+
+        int[] arreglo = new int[size];
+
+        for(int n = 0; n < size ; n++){
+            arreglo[n] = chance.nextInt(100);
+        }
+
+        System.out.println("Arreglo generado: " + Arrays.toString(arreglo));
+        
+        entrada.close();
+
     }
     
 }

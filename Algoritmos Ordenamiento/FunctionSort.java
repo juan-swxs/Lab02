84b4ca5d63aeeabfc999cc9615b51cc712c4e498
juan-swxs <est.juans.munozg@unimilitar.edu.co>
2024-09-01 22:02:58 -0500

    Actualizacion del manejo de System.nanotime() y funciones de Ordenamiento.

--- Algoritmos Ordenamiento/FunctionSort.java
+++ Algoritmos Ordenamiento/FunctionSort.java
@@ -13,4 +13,75 @@ public class FunctionSort {
         }
         return xDoubles;
     }
+
+    public static Double[] insertioSort(Double[] x){
+        int n = x.length;
+        for(int z = 1; z < n; z++){
+            double key = x[z];
+            int j = z - 1;
+            while(j >= 0 && x[j] > key){
+                x[j + 1] = x[j];
+                j--;
+            }
+            x[j + 1] = key;
+        }
+        return x;
+    }
+
+    public static Double[] selectionSort(Double[] array) {
+        int n = array.length;
+    
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            double temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+        
+        return array; 
+    }
+    
+
+    public static Double[] mergeSort(Double[] array) {
+        if (array.length < 2) {
+            return array;
+        }
+        
+        int mid = array.length / 2;
+        Double[] left = new Double[mid];
+        Double[] right = new Double[array.length - mid];
+        
+        System.arraycopy(array, 0, left, 0, mid);
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+    
+        mergeSort(left);
+        mergeSort(right);
+    
+        merge(array, left, right);
+    
+        return array;  
+    }
+    
+    private static void merge(Double[] array, Double[] left, Double[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
 }
--- Algoritmos Ordenamiento/Ordenamientos.java
+++ Algoritmos Ordenamiento/Ordenamientos.java
@@ -1,54 +1,92 @@
 import java.util.Scanner;
-import java.util.Arrays;
 import java.util.Random;
 
 public class Ordenamientos {
 
     int[] temps = {100, 500, 1000, 5000, 10000};
-    private Double[] array;
 
-    public Ordenamientos(){
+    public Ordenamientos() {
         Scanner entrada = new Scanner(System.in);
         int opcion;
 
-       do {
-        System.out.println("\t~Metodos de Ordenamiento~");
-        System.out.println("\t1. Metodo de burbuja.");
-        System.out.println("\t2. Metodo de inserción.");
-        System.out.println("\t3. Metodo de seleccion.");
-        System.out.println("\t4. Metodo de mergesort");
-        System.out.println("\t5. salir");
-        System.out.print("Elija una opcion: ");
-        opcion = entrada.nextInt();
+        do {
+            System.out.println("\t~Metodos de Ordenamiento~");
+            System.out.println("\t 1.Metodo de burbuja.");
+            System.out.println("\t2. Metodo de inserción.");
+            System.out.println("\t3. Metodo de seleccion.");
+            System.out.println("\t4. Metodo de mergesort");
+            System.out.println("\t5. Generar tabla comparativa.");
+            System.out.println("\t6. salir");
+            System.out.print("Elija una opcion: ");
+            opcion = entrada.nextInt();
 
-        switch (opcion) {
-            case 1:
-                System.out.println(Arrays.toString(generateRandoms()));
-                burbuja();
-                break;
-            case 5:
-                System.out.println("...Saliendo del programa....");
-                break;
-            default:
-            System.out.println("...Opcion no valida...");
-                break;
-        }
-        
-       } while (opcion != 5);
+            switch (opcion) {
+                case 1:
+                    for (int size : temps) {
+                        Double[] array = generateRandoms(size);
+                        long startTime = System.nanoTime();
+                        FunctionSort.bubbleSort(array);
+                        long endTime = System.nanoTime();
+                        long duration = (endTime - startTime); 
+                        System.out.println("Burbuja (Tamaño " + size + "): " + duration + " ms");
+                    }
+                    break;
+                case 2:
+                    for (int size : temps) {
+                        Double[] array = generateRandoms(size);
+                        long startTime = System.nanoTime();
+                        FunctionSort.insertioSort(array);
+                        long endTime = System.nanoTime();
+                        long duration = (endTime - startTime);
+                        System.out.println("Inserción (Tamaño " + size + "): " + duration + " ms");
+                    }
+                    break;
+                case 3:
+                    for (int size : temps) {
+                        Double[] array = generateRandoms(size);
+                        long startTime = System.nanoTime();
+                        FunctionSort.selectionSort(array);
+                        long endTime = System.nanoTime();
+                        long duration = (endTime - startTime);
+                        System.out.println("Selección (Tamaño " + size + "): " + duration + " ms");
+                    }
+                    break;
+                case 4:
+                    for (int size : temps) {
+                        Double[] array = generateRandoms(size);
+                        long startTime = System.nanoTime();
+                        FunctionSort.mergeSort(array);
+                        long endTime = System.nanoTime();
+                        long duration = (endTime - startTime);
+                        System.out.println("MergeSort (Tamaño " + size + "): " + duration + " ms");
+                    }
+                    break;
+                case 5:
+                    generateComparativeTable();
+                    break;
+                case 6:
+                    System.out.println("...Saliendo del programa....");
+                    break;
+                default:
+                    System.out.println("...Opcion no valida...");
+                    break;
+            }
+
+        } while (opcion != 6);
 
         entrada.close();
     }
 
-    private Double[] generateRandoms(){
+    private Double[] generateRandoms(int size) {
         Random random = new Random();
-        array = new Double[temps.length];
-        for (int n = 0; n < temps.length; n++){
+        Double[] array = new Double[size];
+        for (int n = 0; n < size; n++) {
             array[n] = random.nextDouble() * 1000;
         }
         return array;
     }
 
-    private void burbuja(){
-        System.out.println(Arrays.toString(FunctionSort.bubbleSort(array)));
+    private void generateComparativeTable() {
+        
     }
 }
